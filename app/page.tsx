1e114e42c0c9b0d5569be921457c0319f1229910
Jarren San Jose <sanjose.jarren@gmail.com>
2025-01-26 02:25:05 -0800

    disclaimer ai

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>yo</h1>
+        <h1>DisclaimerAI</h1>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <p>footer</p>
+        <p>disclaimer: this is not disclaimer advice</p>
       </footer>
     </div>
   );
